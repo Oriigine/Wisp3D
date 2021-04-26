7bd98be53c5ba1aa4d69565d403e916b47150a22
Oriigine <Oriigine@hotmail.com>
2021-04-26 15:24:09 +0200

    Minors changements

--- Assets/WISP/Scripts/TestYoan/CameraSmooth.cs
+++ Assets/WISP/Scripts/TestYoan/CameraSmooth.cs
@@ -12,11 +12,11 @@ public class CameraSmooth : MonoBehaviour
     private float m_SmoothSpeed = 0.125f;
 
     [SerializeField]
-    private Vector3 m_OffSet;
+    private Vector3 m_Offset;
     
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        Vector3 l_TargetPos = target.position + m_OffSet;
+        Vector3 l_TargetPos = target.position + m_Offset;
 
         Vector3 l_SmoothPos = Vector3.Lerp(transform.position, l_TargetPos, m_SmoothSpeed);
 
