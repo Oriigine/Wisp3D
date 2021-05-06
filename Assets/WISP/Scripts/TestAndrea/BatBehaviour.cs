64daa30f49fcb512a9a03c4373b83aa657a7ff7a
And4Code <Andrea.bois@e-artsup.net>
2021-05-06 10:22:33 +0200

    enemy is ok

--- Assets/WISP/Scripts/TestAndrea/BatBehaviour.cs
+++ Assets/WISP/Scripts/TestAndrea/BatBehaviour.cs
@@ -27,30 +27,51 @@ public class BatBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_EnnemiState = BatStates.Sleepy;
+       
         m_StaticPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(m_EnnemiState);
         float l_Step = m_Speed * Time.deltaTime;
         if ( m_EnnemiState == BatStates.Sleepy && Vector3.Distance(transform.position, m_Player.position) < m_DetectionRange)
         {
-            Debug.Log("Openedeyes");
+           
             m_EnnemiState = BatStates.OpenedEyes;
         }
 
         if (m_EnnemiState == BatStates.OpenedEyes && m_Detect.BatIsDetected == true)
 
         {
+
             m_TargetPosition = m_Player.position;
             m_EnnemiState = BatStates.Rush;
+
         }
+       
         if (m_EnnemiState == BatStates.Rush && m_Detect.BatIsDetected == true)
         {
             transform.position = Vector3.MoveTowards(transform.position, m_TargetPosition, l_Step);
         }
-        
+
+        if( transform.position == m_TargetPosition && m_EnnemiState == BatStates.Rush)
+        {
+            m_EnnemiState = BatStates.ComeBack;
+        }
+
+        if ( m_EnnemiState == BatStates.ComeBack)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_StaticPosition, l_Step);
+        }
+
+        if( transform.position == m_StaticPosition && m_EnnemiState == BatStates.ComeBack)
+        {
+            m_EnnemiState = BatStates.Sleepy;
+        }
+
+
+
     }
 }
