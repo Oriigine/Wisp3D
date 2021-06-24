38950d60041a351892cd686920c1e1b1435f9a97
And4Code <Andrea.bois@e-artsup.net>
2021-06-24 14:40:59 +0200

    EndTrigger

--- Assets/WISP/EndFade.cs
+++ Assets/WISP/EndFade.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class EndFade : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_Plane;
+
+    [SerializeField]
+    private Image m_EndPic;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "EndDoor")
         {
-            m_Plane.GetComponent<MeshRenderer>().material.DOFade(1, 2);
             gameObject.GetComponent<Bis_Mondhor_PlayerController>().enabled = false;
+
+            Sequence mySequence = DOTween.Sequence();
+            mySequence.Insert(0, m_Plane.GetComponent<MeshRenderer>().material.DOFade(1, 1f));
+            mySequence.Insert(0.75f, m_EndPic.DOFade(1, 10f));
         }
     }
 }
